4f9302c58495d06b4958edb6abbd75fe3cc584cd
Mykola Mamaiev <mamaievm@ae.com>
2022-04-29 17:10:20 +0300

    Init commit.

---
+++ src/main/java/com/griddynamics/phonebook/phonebookspringmvc/PhoneBookSpringMvcApplication.java
@@ -0,0 +1,14 @@
+package com.griddynamics.phonebook.phonebookspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneBookSpringMvcApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/griddynamics/phonebook/phonebookspringmvc/PhoneBookSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griddynamics.phonebook.phonebookspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
